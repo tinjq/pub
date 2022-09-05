9aecd563b00d202fdb2b95f5b5a086c89414fbbd
tinjq <1967694532@qq.com>
2022-09-05 10:23:02 +0800

    arr

---
+++ arr.js
@@ -0,0 +1,15 @@
+var arr = [
+		{name:"note", pass:"U2FsdGVkX19wQIaAkMR44Yh1wmezYFs5A2YrYcNxmD0="},
+		{name:"outlook", pass:"U2FsdGVkX19ZF7RaPyWahV7g+nP/e50/M1ijosBix37PbHY07Wcc6AbTQ8S7/6o6"},
+		{name:"gmail", pass:"U2FsdGVkX19LOCL/kpV9tRmxcnm1VSr0V9F82+S+vNwaoJR2bVkZjOpVkJ345jEH"},
+		{name:"QQ", pass:"U2FsdGVkX1+bJEocQTgLdR+CRgFx8TeL1RbciWUlLUxCgJYj7V7fxjIv1UToJXDg"},
+		{name:"QQ", pass:"U2FsdGVkX18u/OBhRnIqqdkLHLCuAZhuhOBOfkMJqTauVm+aofCkiZIkMbZLKFA8"},
+		{name:"weichat", pass:"U2FsdGVkX1/Ps/whS//e+c5Qi4KHDfhjeKFzTLgi3rXcRMkAnYbXNrGnJ6VsmxCS"},
+		{name:"weichat", pass:"U2FsdGVkX19BqAV7Mwu8Gy52fDLeRds48L40uefJlLE53I1FhmdCeVLNYOWxP1BI"},
+		{name:"BaiduNetdisk", pass:"U2FsdGVkX181doZcvhDvbXXCYZBdD6gfcTGBZT1lXFY+MXQEzMthuSkG4wOe25z/WnFVomopr31RQs7KTMe+Uw=="},
+		{name:"ifeve", pass:"U2FsdGVkX1/ETUiYTvaU4ePhFTEHMLQE4mTpB7FXYxdjHaxssXksYVu/5jnlC55vIPj0nInej8pHCuzc0ukJcbWxnYTY955mNEuQxkDpBBY="},
+		{name:"aliyun", pass:"U2FsdGVkX19GNpRfoTDJ5q9vSdgFbvNJJnxutjgLBxBF1h8c/a8Z9jkSJZc3Hz8Zf2oL2ZSy5fyTc9vY8A0Cww=="},
+		{name:"docker", pass:"U2FsdGVkX19LftfyzmCL9/2zLFS8bS1zYmgix9XyizbwbotJE0Ra0MijY2JKdFpR"},
+		{name:"gitee", pass:"U2FsdGVkX19fi2sVr1Ehkxdcr4763GcxjT+2/cXLrcx3BRMbMVEX+a4ETBF3tn+VHEAvVMm0Rd6T9eQVdW7Zmw=="},
+		{name:"setupvpn", pass:"U2FsdGVkX19diQHGoRY56U8cX+GJRA1DwcoKcfNZdOfVu1JxV5rA/v/qLOzOU8TsKse2kq3+dgMAdxaSqleQIQ=="}
+	];
